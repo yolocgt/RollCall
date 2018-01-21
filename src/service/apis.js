aab9658b850a9cce0d2d990211e282f40f0396c5
yolocgt <1058520616@qq.com>
2018-01-22 00:53:59 +0800

    complete

--- src/service/apis.js
+++ src/service/apis.js
@@ -4,8 +4,23 @@ import c_Api from './api_common';
 // 把所有的axios请求都封装到这个文件中
 
 // 创建axios请求
-var ApiAdmin = new c_Api('admin');
-var ApiCourse = new c_Api('course');
+var ApiAdmin = new c_Api('admin');				//管理员
+var ApiCourse = new c_Api('course');			//课程
+var ApiFaculty = new c_Api('faculty');			//学院
+var ApiMajor = new c_Api('major');				//专业
+var ApiClassInfo = new c_Api('classInfo');		//班级
+var ApiStudent = new c_Api('student');			//学生
+var ApiHeadteacher = new c_Api('headteacher');	//辅导员
+var ApiTeacher = new c_Api('teacher');			//教师
 
 // 导出
-export { ApiAdmin, ApiCourse }
+export {
+	ApiAdmin,
+	ApiCourse,
+	ApiFaculty,
+	ApiMajor,
+	ApiClassInfo,
+	ApiStudent,
+	ApiHeadteacher,
+	ApiTeacher
+}
